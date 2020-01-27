6e402cedf8ac261aba4d3801d628fb0159c53346
erikacy <erika.cy@gmail.com>
2020-01-27 16:57:26 -0500

    fix spacing and factory bot full name testing

--- db/migrate/20200127161548_devise_create_users.rb
+++ db/migrate/20200127161548_devise_create_users.rb
@@ -16,9 +16,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       ## Rememberable
       t.datetime :remember_created_at
 
-
-
-
       ## Trackable
       # t.integer  :sign_in_count, default: 0, null: false
       # t.datetime :current_sign_in_at
--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -2,6 +2,8 @@ require 'factory_bot'
 
 FactoryBot.define do
   factory :user do
+    sequence(:first_name) {|n| "John" }
+    sequence(:last_name) {|n| "Smith" }
     sequence(:email) {|n| "user#{n}@example.com" }
     password { 'password' }
     password_confirmation { 'password' }

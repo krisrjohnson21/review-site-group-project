792d806a04a5c2ac4aa95a1f2ab9c792779f7143
Kris Johnson <krisrjohnson@gmail.com>
2020-01-30 17:13:30 -0500

    model tests written for user, cape, and review

---
+++ spec/models/cape_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+describe Cape do
+  it { should have_many :reviews }
+
+  it { should have_valid(:name).when("a name") }
+  it { should_not have_valid(:name).when(nil, "") }
+
+  it { should have_valid(:full_name).when("a full name") }
+  it { should_not have_valid(:full_name).when(nil, "") }
+
+  it { should have_valid(:gender).when("a gender") }
+  it { should_not have_valid(:gender).when(nil, "") }
+
+  it { should have_valid(:affiliation).when("an affiliation") }
+  it { should_not have_valid(:affiliation).when(nil, "") }
+
+  it { should have_valid(:intelligence).when(50) }
+  it { should_not have_valid(:intelligence).when(nil) }
+
+  it { should have_valid(:strength).when(50) }
+  it { should_not have_valid(:strength).when(nil) }
+
+  it { should have_valid(:speed).when(50) }
+  it { should_not have_valid(:speed).when(nil) }
+
+  it { should have_valid(:url).when("a url string") }
+  it { should_not have_valid(:url).when(nil, "") }
+end

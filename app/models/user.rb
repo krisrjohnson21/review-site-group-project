f5e3a66b281d1bc016ac122af372e26b61ee0021
Kris Johnson <krisrjohnson@gmail.com>
2020-02-04 09:49:39 -0500

    removed commented out line in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :reviews        
-  # has_many :capes
+  has_many :reviews
   validates :first_name, presence: true
   validates :last_name, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 c1 = Cape.create(name: "Nedzilla", full_name: "Debbie Lehman", gender: "Male", affiliation: "Starbucks Galactica", intelligence: 100, strength: 77, speed: 89, url: "https://s3-prod.adage.com/s3fs-public/styles/width_1024/public/little_debbie.jpg")
 c2 = Cape.create(name: "Speedy", full_name: "Jimmy Buffet", gender: "Male", affiliation: "West Side", intelligence: 99, strength: 99, speed: 4, url: "https://www.nationalgeographic.com/content/dam/magazine/rights-exempt/2017/10/Departments/Explore/explore-slow-loris-photo-ark.ngsversion.1503078357493.adapt.1900.1.jpg")
 c3 = Cape.create(name: "Red Rover", full_name: "Keyshawn Johnson", gender: "Male", affiliation: "Tree Napper", intelligence: 4, strength: 100, speed: 7, url: "https://s3.amazonaws.com/ogden_images/www.minotdailynews.com/images/2019/09/06224333/7-Panda1-560x840.jpg")
@@ -12,4 +11,3 @@ r3 = Review.create(rating: 1, body: "Only has two shirts, wears one.", user_id: 
 r4 = Review.create(rating: 10, body: "Thinks he moves fast, and everyone else moves slow", user_id: 1, cape_id: 2)
 r5 = Review.create(rating: 5, body: "Still sleeping", user_id: 2, cape_id: 3)
 r6 = Review.create(rating: 7, body: "Eats lunch alone, when he doesnt have to", user_id: 3, cape_id: 3)
-

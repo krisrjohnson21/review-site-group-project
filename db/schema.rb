d4a87101ee1f48f4fa7d846814926742241ea3ce
Kris Johnson <krisrjohnson@gmail.com>
2020-02-05 13:59:46 -0500

    Completed CarrierWave

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,6 @@ ActiveRecord::Schema.define(version: 2020_02_04_181756) do
   enable_extension "plpgsql"
 
   create_table "capes", force: :cascade do |t|
-    t.bigint "user_id", null: false
     t.string "name", null: false
     t.string "full_name", null: false
     t.string "gender", null: false
@@ -28,7 +27,6 @@ ActiveRecord::Schema.define(version: 2020_02_04_181756) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_capes_on_name", unique: true
-    t.index ["user_id"], name: "index_capes_on_user_id"
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -52,7 +50,6 @@ ActiveRecord::Schema.define(version: 2020_02_04_181756) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "role", default: "member", null: false
     t.string "profile_photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
